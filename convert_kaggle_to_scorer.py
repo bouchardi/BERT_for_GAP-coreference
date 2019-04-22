d53632a371a8c26ef86dbfa8d8b6f9c3bcbcb7dd
k1c <cpelletier@klipfolio.com>
2019-04-22 02:58:42 -0400

    adding results

--- convert_kaggle_to_scorer.py
+++ convert_kaggle_to_scorer.py
@@ -2,14 +2,13 @@
 Use this script to convert kaggle submission to file needed for gap_scorer.py
 
 Usage of gap_scorer.py:
-    python3 gap_scorer.py --gold_tsv gap-test.tsv --system_tsv system.tsv
-
+    python3 gap_scorer.py --gold_tsv gap-test.tsv --system_tsv ../results/gap_scorer-test.tsv
 '''
 
 import pandas as pd
 
 # read in kaggle submission
-df = pd.read_csv('../kaggle_gap-test.tsv', delimiter='\t', encoding='utf-8')
+df = pd.read_csv('../results/kaggle-dev.tsv', delimiter='\t', encoding='utf-8')
 
 # convert df to nparray
 df_values = df[['A','B','NEITHER']].values
@@ -27,5 +26,5 @@ df_ = pd.DataFrame(data=nparray, columns=columns)
 df_.insert(loc=0, column='ID', value=df['ID'])
 
 # save to file
-df_.to_csv("system.tsv", sep='\t', encoding='utf-8', index=False)
+df_.to_csv("../results/gap_scorer-dev.tsv", sep='\t', encoding='utf-8', index=False)
 
